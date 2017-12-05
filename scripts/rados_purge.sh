995094c9099f714bddf5e9ce1552bec140c2363f
root <root@li458-103.members.linode.com>
2017-12-05 15:09:46 +0000

    fixed purging of buckets also

--- scripts/rados_purge.sh
+++ scripts/rados_purge.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 
 rados purge default.rgw.data.root --yes-i-really-really-mean-it &
-rados purge default.rgw.buckets.index --yes-i-really-really-mean-it &
 rados purge default.rgw.buckets.data --yes-i-really-really-mean-it &
+rados purge default.rgw.buckets.index --yes-i-really-really-mean-it &
+rados purge default.rgw.meta --yes-i-really-really-mean-it &
+rados purge default.rgw.users.uid --yes-i-really-really-mean-it &
+rados purge default.rgw.gc --yes-i-really-really-mean-it &
+rados purge default.rgw.log --yes-i-really-really-mean-it &
+
+sleep 2
+echo "waitingi for completion..."
+wait
 
