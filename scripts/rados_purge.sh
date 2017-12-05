331bafb3fcce922d8bf2798e51e9ac35d93955b7
root <root@li458-103.members.linode.com>
2017-12-05 19:21:18 +0000

    fixed purging bucket data

--- scripts/rados_purge.sh
+++ scripts/rados_purge.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
+ceph pg set_full_ratio 0.99
 
 rados purge default.rgw.data.root --yes-i-really-really-mean-it &
 rados purge default.rgw.buckets.data --yes-i-really-really-mean-it &
 rados purge default.rgw.buckets.index --yes-i-really-really-mean-it &
 rados purge default.rgw.meta --yes-i-really-really-mean-it &
 rados purge default.rgw.users.uid --yes-i-really-really-mean-it &
-rados purge default.rgw.gc --yes-i-really-really-mean-it &
-rados purge default.rgw.log --yes-i-really-really-mean-it &
+rados purge default.rgw.users.swift --yes-i-really-really-mean-it &
 
 sleep 2
+rados purge default.rgw.gc --yes-i-really-really-mean-it
+rados purge default.rgw.log --yes-i-really-really-mean-it
+
+ceph pg set_full_ratio 0.95
+
 echo "waitingi for completion..."
 wait
 
--- scripts/swift_init.sh
+++ scripts/swift_init.sh
@@ -1,3 +1,10 @@
-./bin/radosgw-admin user create --uid="cosbench" --display-name="cosbench"
-./bin/radosgw-admin user modify --uid=cosbench --max-buckets=0
-./bin/radosgw-admin subuser create --uid=cosbench --subuser=cosbench:operator --secret=redhat --access=full --key-type=swift
+#!/bin/bash -x 
+
+if [ -f ./bin/radosgw-admin ]; then
+	RA=./bin/radosgw-admin
+else
+	RA=radosgw-admin
+fi
+$RA user create --uid="cosbench" --display-name="cosbench" | jq
+$RA user modify --uid=cosbench --max-buckets=0 | jq
+$RA subuser create --uid=cosbench --subuser=cosbench:operator --secret=redhat --access=full --key-type=swift | jq
