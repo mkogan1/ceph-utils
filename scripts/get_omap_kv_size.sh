cd1cbb5affaea20dc40a59acebc3505c1f416e4c
mkogan <mkogan@redhat.com>
2017-12-24 15:00:08 +0000

    parse keys correctly

--- scripts/get_omap_kv_size.sh
+++ scripts/get_omap_kv_size.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
 CEPH_DIR=${1:-"./bin"}
+if [  -f "${CEPH_DIR}/rados" ]; then
+    CEPH_DIR="${CEPH_DIR}/"
+else
+	CEPH_DIR=""
+fi
 
 pool=${2:-"default.rgw.buckets.index"}
-list=`$CEPH_DIR/rados -p $pool ls`
+list=`${CEPH_DIR}rados -p $pool ls`
 
 (
 echo "object size_keys(kB) size_values(kB) total(kB) nr_keys nr_values"
 for obj in $list; do
-    echo -en "$obj ";
-    $CEPH_DIR/rados -p $pool listomapvals $obj | awk '
-    /^key: \(.* bytes\)/ { sizekey+= substr($2, 2, length($2)); nbkeys++ }
-    /^value: \(.* bytes\)/ { sizevalue+= substr($2, 2, length($2)); nbvalues++ }
-    END { printf("%i %i %i %i %i\n", sizekey/1024, sizevalue/1024, (sizekey+sizevalue)/1024, nbkey, nbvalues) }
-'
+    RES=$(${CEPH_DIR}rados -p $pool listomapvals $obj | awk '
+    /^value \(.* bytes\)/ { sizevalues+= substr($2, 2, length($2)); nbvalues++ }
+    END { printf("%i %i\n", sizevalues, nbvalues) }
+    ')
+    SIZEVALUES=$(echo $RES | cut -f1 -d' ')
+    NUMVALUES=$(echo $RES | cut -f2 -d' ')
+
+    RES=$(${CEPH_DIR}rados -p $pool listomapkeys $obj | awk '
+    { sizekeys+=length(); nbkeys++ }
+    END { printf("%i %i\n", sizekeys, nbkeys) }
+    ')
+    SIZEKEYS=$(echo $RES | cut -f1 -d' ')
+    NUMKEYS=$(echo $RES | cut -f2 -d' ')
+
+    echo -e "${obj} $((SIZEKEYS/1024)) $((SIZEVALUES/1024)) $(( (SIZEKEYS+SIZEVALUES)/1024 )) ${NUMKEYS} ${NUMVALUES}"
 done
 )
