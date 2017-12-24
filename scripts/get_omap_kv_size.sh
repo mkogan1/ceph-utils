e702fc111ceaf45733fe1cb462a3f9884c30ebf6
Mark Kogan <mkogan@redhat.com>
2017-12-24 08:30:45 -0500

    improovmetns

--- scripts/get_omap_kv_size.sh
+++ scripts/get_omap_kv_size.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
 
-pool=${1:-"default.rgw.buckets.index"}
-list=`rados -p $pool ls`
+CEPH_DIR=${1:-"./bin"}
+
+pool=${2:-"default.rgw.buckets.index"}
+list=`$CEPH_DIR/rados -p $pool ls`
 
 (
 echo "object size_keys(kB) size_values(kB) total(kB) nr_keys nr_values"
 for obj in $list; do
     echo -en "$obj ";
-    rados -p $pool listomapvals $obj | awk '
+    $CEPH_DIR/rados -p $pool listomapvals $obj | awk '
     /^key: \(.* bytes\)/ { sizekey+= substr($2, 2, length($2)); nbkeys++ }
     /^value: \(.* bytes\)/ { sizevalue+= substr($2, 2, length($2)); nbvalues++ }
     END { printf("%i %i %i %i %i\n", sizekey/1024, sizevalue/1024, (sizekey+sizevalue)/1024, nbkey, nbvalues) }
