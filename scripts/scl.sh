5f233fc7af69c2c92d83bdaa0f890d405f9426e5
Mark Kogan <mkogan@redhat.com>
2018-12-25 14:59:24 +0200

    will 1. update scl script 2. add versioning stresstest script

--- scripts/scl.sh
+++ scripts/scl.sh
@@ -1,2 +1,2 @@
-#scl enable devtoolset-7 llvm-toolset-7 rh-git29 -- $SHELL
-scl enable devtoolset-7 rh-git29 -- $SHELL
+scl enable devtoolset-7 llvm-toolset-7 rh-git29 -- $SHELL
+#scl enable devtoolset-7 rh-git29 -- $SHELL
---
+++ scripts/versioning_loop.sh
@@ -0,0 +1,54 @@
+set -x
+while true; do
+
+  ### - create bucket
+  aws --endpoint-url http://localhost:8000 s3 mb s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef
+
+  # flags ref: https://bugzilla.redhat.com/show_bug.cgi?id=1658308#c6
+  radosgw-admin bucket list
+  radosgw-admin metadata get bucket.instance:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef:$(radosgw-admin metadata get  bucket:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef 2>/dev/null | jq -r ".data.bucket.bucket_id") 2>/dev/null | jq . | egrep "flags|ver"
+
+  ### - enable versioning on the bucket
+  aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Enabled
+
+  aws --endpoint-url http://localhost:8000 s3api get-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef
+  radosgw-admin metadata get bucket.instance:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef:$(radosgw-admin metadata get  bucket:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef 2>/dev/null | jq -r ".data.bucket.bucket_id") 2>/dev/null | jq . | egrep "flags|ver"
+
+  ### - Upload initial_file.txt
+  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  aws --endpoint-url http://localhost:8000 s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
+
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+
+  ### - Upload new version of initial_file.txt
+  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  aws --endpoint-url http://localhost:8000 s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
+
+  aws --endpoint-url http://localhost:8000 s3api list-object-versions --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --prefix initial_file.txt
+
+  ### - Delete initial_file.txt
+  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+  aws --endpoint-url http://localhost:8000 s3 rm s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+
+  ### - Upload v2_initial_file.txt
+  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  aws --endpoint-url http://localhost:8000 s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/v2_initial_file.txt
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+
+  ### - Delete v2_initial_file.txt
+  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url http://localhost:8000 s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+  aws --endpoint-url http://localhost:8000 s3 rm s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/v2_initial_file.txt
+  aws --endpoint-url http://localhost:8000 s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
+
+
+  ### Customer is attempting to remove bucket with following command:
+  radosgw-admin bucket list
+  radosgw-admin bucket stats --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef
+  # comment to increase ver count
+  #radosgw-admin bucket rm --bucket=redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --purge-objects
+  #radosgw-admin bucket stats --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef
+
+done
