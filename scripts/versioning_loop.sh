901560655d1b3959993cefd6bd46277f2b95bab7
mkogan1 <mkogan@redhat.com>
2018-12-30 07:40:14 -0500

    add randomness factor config

--- scripts/versioning_loop.sh
+++ scripts/versioning_loop.sh
@@ -4,6 +4,8 @@ set -x
 # pip install awscli
 # aws configure
 
+CHANCE=4
+
 #EPURL=http://localhost:8000
 EPURL=http://172.16.0.6:8000
 
@@ -26,30 +28,30 @@ while true; do
   radosgw-admin metadata get bucket.instance:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef:$(radosgw-admin metadata get  bucket:redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef 2>/dev/null | jq -r ".data.bucket.bucket_id") 2>/dev/null | jq . | egrep "flags|ver"
 
   ### - Upload initial_file.txt
-  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  if [[ $(( RANDOM % ${CHANCE} )) -eq 0 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
   aws --endpoint-url ${EPURL} s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
 
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
 
   ### - Upload new version of initial_file.txt
-  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  if [[ $(( RANDOM % ${CHANCE} )) -eq 0 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
   aws --endpoint-url ${EPURL} s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
 
   aws --endpoint-url ${EPURL} s3api list-object-versions --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --prefix initial_file.txt
 
   ### - Delete initial_file.txt
-  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  if [[ $(( RANDOM % ${CHANCE} )) -eq 0 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
   aws --endpoint-url ${EPURL} s3 rm s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/initial_file.txt
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
 
   ### - Upload v2_initial_file.txt
-  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  if [[ $(( RANDOM % ${CHANCE} )) -eq 0 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
   aws --endpoint-url ${EPURL} s3 cp ~/tmp/100MB-rand.dat s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/v2_initial_file.txt
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
 
   ### - Delete v2_initial_file.txt
-  if [[ $(( RANDOM % 2 )) -eq 1 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
+  if [[ $(( RANDOM % ${CHANCE} )) -eq 0 ]]; then aws --endpoint-url ${EPURL} s3api put-bucket-versioning --bucket redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --versioning-configuration Status=Suspended ; fi
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
   aws --endpoint-url ${EPURL} s3 rm s3://redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef/v2_initial_file.txt
   aws --endpoint-url ${EPURL} s3 ls redhat_testing_bucket_bea48dac-e74a-11e8-8d41-f85971b8e3ef --human
