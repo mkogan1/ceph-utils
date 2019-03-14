271eebf1eb53e47af0ebb04a86afe9447fb40234
Mark Kogan <mkogan@redhat.com>
2019-03-14 05:41:41 -0400

    narrowing the reproducer

--- scripts/hadoop_tmp_mon.sh
+++ scripts/hadoop_tmp_mon.sh
@@ -1,13 +1,20 @@
-#!/bin/bash -x
-
+#!/bin/bash 
+set -x
 while true; do
-  TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | wc -l)
+  #TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | wc -l)
+  TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | grep 00131 | wc -l)
+  date
   echo "TC=$TC"
   if [ "$TC" -gt 0 ]; then 
-    ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill -9
+    #sleep 1
+    ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill  -9
     sleep 4 
     ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill -9
+
+
+    exit
   fi
+  sleep 1
 done
 
 
