a0c16219088502a75e9edc81a4bfb78cd1302ed5
Mark Kogan <mkogan@redhat.com>
2019-03-18 10:22:16 -0400

    fixes

--- scripts/hadoop_tmp_mon.sh
+++ scripts/hadoop_tmp_mon.sh
@@ -2,15 +2,15 @@
 set -x
 while true; do
   #TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | wc -l)
-  TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | grep 00131 | wc -l)
+  TC=$(s3cmd ls -r --human s3://hadoop01 | grep attempt | grep part | grep $1 | wc -l)
   date
   echo "TC=$TC"
   if [ "$TC" -gt 0 ]; then 
     #sleep 1
-    ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill  -9
-    sleep 4 
-    ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill -9
-
+    # ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill  -9
+    # sleep 5
+    # ps -ef | grep "[m]apreduce" | grep -v bash | awk '{ print $2 }' | xargs --verbose kill -9
+    hadoop job -kill $2
 
     exit
   fi
