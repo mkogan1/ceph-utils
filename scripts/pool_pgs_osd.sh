3259385a00ff462ce74b0cbc9b9fddb24ba1ab9a
Mark Kogan <mkogan@redhat.com>
2019-03-26 11:09:10 -0400

    1st complete ver

--- scripts/pool_pgs_osd.sh
+++ scripts/pool_pgs_osd.sh
@@ -21,20 +21,22 @@ rm $TMPF
 echo 
 SIZE=$(ceph osd dump 2>/dev/null | grep "pool $POOL_NUM" | awk '{ print $6 }')
 NDEVS=`ceph osd ls 2>/dev/null | wc -l`
+echo -e "\n# PRIMARY and Replicated/EC PGs number per OSD sorted by total descending:"
 ceph pg dump 2>/dev/null | grep "^$POOL_NUM\." | awk "{ \
   seps[1]=\",\"; split(substr(\$17, 2, length(\$17)-2), res, seps[1]); \
-  for (s = 0 ; s < $SIZE ; s++) { dist[s][res[s]]++; }
+  for (s = 1 ; s <= $SIZE ; s++) { dist[s][res[s]]++; }
 } END { \
   min=65535; max=0; sump=0; \
   for (i = 0 ; i < $NDEVS ; i++) { 
-    printf(\" OSD%d PGs: %d %d %d total: %d\n\", i, dist[1][i], dist[2][i], dist[3][i], dist[1][i]+dist[2][i]+dist[3][i]); \
-    printf(\"osd.%d\tPGs: %d %d %d total: %d\n\", i, dist[1][i], dist[2][i], dist[3][i], dist[1][i]+dist[2][i]+dist[3][i]); \
-    for (s = 0 ; s < $SIZE ; s++) { 
+    printf(\"osd.%d\tPGs: \", i); \
+    tot = 0; \
+    for (s = 1 ; s <= $SIZE ; s++) { printf(\"% 4d \", dist[s][i]); tot+=dist[s][i]; }
+    printf(\" ,total=% 4d\n\", tot)
     if (min > dist[1][i]) {min = dist[1][i]} \
     if (max < dist[1][i]) {max = dist[1][i] \
   } \
   sump+=dist[1][i]}; ratio=\"NaN\"; \
   if (min > 0){ratio=max/min}; \
   printf( \" Primary: Min=%d Max=%d Ratio=%s Average=%s Max/Ave=%s\n\", min,max,ratio,sump/$NDEVS,max/(sump/$NDEVS) )
-}"
+}" | sort -n -r -k 7,7
 
