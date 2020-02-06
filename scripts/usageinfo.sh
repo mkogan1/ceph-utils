b8101eab0e14e39f15a79d3c1bb7834dc72c5aaf
Mark Kogan <mkogan@redhat.com>
2020-02-06 16:46:29 +0200

    add resapi usage script

---
+++ scripts/usageinfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+token=b2345678901234567890 ## USER_TOKEN
+secret=b234567890123456789012345678901234567890 ## USER_SECRET
+query=$1
+bucket=$2
+query3="&uid="
+query4="&bucket="
+#query2=admin/usage
+query2=admin/bucket
+#date=$(for i in $(date "+%H") ; do date "+%a, %d %b %Y $(( 10#$i-8 )):%M:%S +0000" ; done)
+date=$(date -Ru)
+header="GET\n\n\n${date}\n/${query2}"
+sig=$(echo -en ${header} | openssl sha1 -hmac ${secret} -binary | base64)
+#curl -v -H "Date: ${date}" -H "Authorization: AWS ${token}:${sig}" -L -X GET "http://127.0.0.1:8000/${query2}?format=json${query3}${query}" -H "Host: 127.0.0.1:8000"
+curl -v -H "Date: ${date}" -H "Authorization: AWS ${token}:${sig}" -L -X GET "http://127.0.0.1:8000/${query2}?format=json${query3}${query}${query4}${bucket}&stats=true" -H "Host: 127.0.0.1:8000"
+
